e6dac453f9e8f509915315a0a0359630866288ca
Hossam <hossamfathi050@outlook.com>
2021-07-12 23:58:37 +0300

    Add new model

--- project/ad/models.py
+++ project/ad/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Ad(models.Model):
+    name = models.CharField(max_length=50)
+    price=models.IntegerField()
+    content=models.TextField()
