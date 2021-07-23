837de63d408a0c98928c33d04a8f8453bf185e5f
Hossam <hossamfathi050@outlook.com>
2021-07-23 17:14:18 +0300

    Add Test

--- project/ad/models.py
+++ project/ad/models.py
@@ -9,3 +9,16 @@ class Ad(models.Model):
     def __str__(self):
         return self.name
         
+
+
+from django.db import models
+
+# Create your models here.
+class Ad(models.Model):
+    name = models.CharField(max_length=50)
+    price=models.IntegerField()
+    content=models.TextField()
+ 
+    def __str__(self):
+        return self.name
+        
