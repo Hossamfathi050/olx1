a5497bc52d9f5229222dc22baeb6edc9aed4444c
Hossam <hossamfathi050@outlook.com>
2021-07-25 13:22:46 +0300

    Add users model

--- project/users/models.py
+++ project/users/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    name = models.CharField(max_length=50)
-    price=models.IntegerField()
-    content=models.TextField()
+    fullname = models.CharField(max_length=250)
+    email= models.EmailField( max_length=254)
+    phone = models.CharField(max_length=12)
+    mobile = models.CharField(max_length=12)
+    address = models.CharField(max_length=75)
  
     def __str__(self):
-        return self.name
+        return self.fullname
         
