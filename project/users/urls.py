e93c7335db948de0d9cad3ca1a97085e471c38a0
Hossam <hossamfathi050@outlook.com>
2021-07-25 13:27:22 +0300

    Add users admin

--- project/users/urls.py
+++ project/users/urls.py
@@ -7,7 +7,7 @@ from django.urls import path
 app_name = 'users'
 
 urlpatterns = [
-    path('users/',name='users'),
-
+    # path('users/',name='users'),
+    # path('insert' , views.Sc_details_add,name='insert'),
 
 ]
