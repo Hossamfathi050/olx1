ed0cb86f5872c1212b8eccba823dc03793edcd86
Hossam <hossamfathi050@outlook.com>
2021-07-25 13:18:25 +0300

    Add users app urls

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,18 +1,13 @@
- url app 
+
 from django.urls import path
-from . import views
-from .views import Sc_detailsList
-
-from django.conf.urls import url
 
 
-app_name = 'sc_details'
+
+
+app_name = 'users'
 
 urlpatterns = [
-    path('' , views.Sc_detailsList.as_view(),name='sc_details_list'),
+    path('users/',name='users'),
 
-    path('insert' , views.Sc_details_add,name='insert'),
-
- 
 
 ]
