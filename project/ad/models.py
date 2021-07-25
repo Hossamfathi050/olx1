a425ff4b1210f2cdd0eab42443fd1a10fc12e375
Hossam <hossamfathi050@outlook.com>
2021-07-25 13:19:29 +0300

    Add users app urls

--- project/ad/models.py
+++ project/ad/models.py
@@ -10,15 +10,3 @@ class Ad(models.Model):
         return self.name
         
 
-
-from django.db import models
-
-# Create your models here.
-class Ad(models.Model):
-    name = models.CharField(max_length=50)
-    price=models.IntegerField()
-    content=models.TextField()
- 
-    def __str__(self):
-        return self.name
-        
