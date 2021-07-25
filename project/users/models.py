c5128ba74074aaf726f2f1bcf4d1ed01f0a9d248
Hossam <hossamfathi050@outlook.com>
2021-07-25 13:20:32 +0300

    Add users app urls

--- project/users/models.py
+++ project/users/models.py
@@ -1,3 +1,12 @@
+
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=50)
+    price=models.IntegerField()
+    content=models.TextField()
+ 
+    def __str__(self):
+        return self.name
+        
